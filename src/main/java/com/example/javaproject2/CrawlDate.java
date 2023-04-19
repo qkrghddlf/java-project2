1b9e343712bb2a303b71691fb23e60e2a8f7d312
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-19 13:10:33 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/CrawlDate.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2;
+
+import java.time.LocalDateTime lastCrawlDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+    // alt + insert
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
+
