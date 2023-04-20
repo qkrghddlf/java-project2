33a623625bddcff89a72b4df76c2b55d6ec17b60
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-20 13:37:16 +0900

    feat: Format에 대한 연습 -float

---
+++ src/main/java/com/example/javaproject2/week2/FloatFormatEx.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week2;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 0.5);
+        System.out.printf("%.1f\n", 3.14);
+        System.out.printf("%.2f\n", 3.141);
+    }
+}
