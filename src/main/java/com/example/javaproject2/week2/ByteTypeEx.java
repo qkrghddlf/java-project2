3a271c9754a9d4521553ce97aad85b67abb46c30
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-20 14:37:59 +0900

    feat: Format Type Exercise - int - byte

---
+++ src/main/java/com/example/javaproject2/week2/ByteTypeEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week2;
+
+public class ByteTypeEx {
+    public static void main(String[] args) {
+        byte btype1 = 10;
+        byte btype2 = -110;
+        byte btype3 = -128;
+        byte btype4 = 127;
+    }
+}
