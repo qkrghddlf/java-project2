9df2e2a7bdf80f2ac3d3cbb3314a124900454638
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-20 09:41:35 +0900

    feat: ScannerStrEx

---
+++ src/main/java/com/example/javaproject2/week2/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week2;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    static void inputStrAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week2;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        ScannerStrEx.inputStrAndPlus();
+    }
+}
