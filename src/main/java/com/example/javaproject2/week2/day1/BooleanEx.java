69a0fdb8ae6163d83691f1ea001279cb65bec176
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-24 14:36:49 +0900

    feat: Boolean 실습

---
+++ src/main/java/com/example/javaproject2/week2/day1/BooleanEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.day1;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean isBalanceSufficient = true;
+        boolean result1 = 1 == 1;
+        boolean result2 = 2 == 3;
+        System.out.println(isBalanceSufficient);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
