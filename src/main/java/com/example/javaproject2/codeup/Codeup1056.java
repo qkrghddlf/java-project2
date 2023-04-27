0304da585a672eeee87b98e5071e275c684bd5e6
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-27 13:32:38 +0900

    feat: Codeup 1053~1055 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1056.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+
+    }
+}
