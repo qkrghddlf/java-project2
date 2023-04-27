86c9dd92bcc38721a01e722e573280ab376b7e1e
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-27 11:13:06 +0900

    feat: Codeup1049~1052 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1049.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1049 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] tVal = sc.nextLine().split(" ");
+        System.out.println(Integer.parseInt(tVal[0]) > Integer.parseInt(tVal[1]) ? 1: 0);
+
+    }
+}
