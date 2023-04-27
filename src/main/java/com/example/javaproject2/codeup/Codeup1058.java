d82a1410cab0c76863ec0c7ea8e249c7effbbf1a
DESKTOP-ECF52QA\user <redone1998@naver.com>
2023-04-27 14:30:49 +0900

    feat: Codeup 1047~1048, 1056~1058 풀이

--- src/main/java/com/example/javaproject2/codeup/Codeup1058.java
+++ src/main/java/com/example/javaproject2/codeup/Codeup1058.java
@@ -2,11 +2,13 @@ package com.example.javaproject2.codeup;
 
 import java.util.Scanner;
 
-public class Codeup1056 {
+public class Codeup1058 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        boolean result = sc.nextInt() != sc.nextInt();
+        boolean result1 = sc.nextInt() == 0;
+        boolean result2 = sc.nextInt() == 0;
+        boolean result = result1 && result2;
         System.out.printf("%d\n", result ? 1 : 0);
 
     }
